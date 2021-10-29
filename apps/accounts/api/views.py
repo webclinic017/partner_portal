8aec84583faf8bc51f2aeb7aba20a2a4411a4fdd
Estevao Viana <31069416+estevaomrviana@users.noreply.github.com>
2021-10-29 19:39:05 -0300

    ajustando retorno em api

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -3,18 +3,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..models import CustomerUser
 from .serializers import CustomerUserSerializer
+from django.http import HttpResponse
 
 
 @api_view(['GET', 'POST'])
 def auth(request):
 
     if request.method == 'GET':
-        return Response("3_acessos", status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
     
     if request.method == 'POST':
         data = request.data
         if CustomerUser.objects.filter(username = data['username'], password = data['password']).count() >= 1:
-            # return Response(status=status.HTTP_200_OK)
-            return Response("3_acessos", status=status.HTTP_200_OK)
+            return HttpResponse('3_acessos', status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
